30ee37e49ec07922d475fd698f5f74478f0e9187
anmolpal2001 <anmolpal2001@gmail.com>
2024-04-13 01:32:40 +0530

    update index.js

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ console.log(process.env.NODE_ENV);
   // app.get("*", (req, res) => {
   //   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
   // });
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
-  });
+  // app.use(express.static(path.join(__dirname, "../client/dist")));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
+  // });
 // }
 
 
