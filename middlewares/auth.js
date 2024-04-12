26c4ec6fe79907034fad994ec2bfa0147d857696
anmolpal2001 <anmolpal2001@gmail.com>
2024-04-13 01:55:32 +0530

    update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
     try{
         const token = req.body.token || req.cookies.token || req.headers.authorization.split(" ")[1];
         if(!token || token == undefined || token == "")
-        {z
+        {
             return res.status(401).json({
                 success : false,
                 message : "Token missing",
